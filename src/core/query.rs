d961711db6f031924594cbffcf3e68709e934144
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-06-22 00:00:36 +0800

    style: normalize query tests

--- src/core/query.rs
+++ src/core/query.rs
@@ -1,24 +1,24 @@
 pub fn make_fts_query(query: &str) -> String {
     query
         .split_whitespace()
-        .map(|token| format!("\"{}\"", token.replace('"', "\"\"")))
+        .map(|term| format!("\"{}\"", term.replace('"', "\"\"")))
         .collect::<Vec<_>>()
         .join(" ")
 }
 
-#[cfg(test)]
 #[rustfmt::skip]
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn wraps_single_token_in_quotes() {
+    fn wraps_single_term_in_quotes() {
         let actual = make_fts_query("payment");
         assert_eq!(actual, "\"payment\"");
     }
 
     #[test]
-    fn preserves_dash_inside_quoted_token() {
+    fn preserves_dash_inside_quoted_term() {
         let actual = make_fts_query("payment-service");
         assert_eq!(actual, "\"payment-service\"");
     }
@@ -31,8 +31,8 @@ mod tests {
 
     #[test]
     fn escapes_embedded_double_quotes() {
-        let actual = make_fts_query("mock \"token\"");
-        let expected = "\"mock\" \"\"\"token\"\"\"";
+        let actual = make_fts_query("mock \"value\"");
+        let expected = "\"mock\" \"\"\"value\"\"\"";
         assert_eq!(actual, expected);
     }
 
